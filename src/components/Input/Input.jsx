fee102301796449c2ca4e9c5e2203170330ac3b8
Daniel Alves <dalima.dev@gmail.com>
2022-05-11 23:16:06 -0300

    input feito

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import "./Input.css"
+import React, { useState } from "react";
+import "./Input.css";
 
 function Input({ create }) {
+  const [descricao, setDescricao] = useState("");
+  const [valor, setValor] = useState(0);
+
+  const objetivo = {
+    descricao,
+    valor,
+    atingido: false,
+  };
+
   return (
-    <div id='input'>
-        <input type="text" name="descricao" id="" placeholder='Descrição' />
-        <input type="number" name="valor" id="" placeholder='Valor' />
-        <input type="button" value="Cadastrar" />
+    <div id="input">
+      <input
+        type="text"
+        name="descricao"
+        id=""
+        placeholder="Descrição"
+        onChange={(e) => setDescricao(e.target.value)}
+      />
+      <input
+        type="number"
+        name="valor"
+        id=""
+        placeholder="Valor"
+        onChange={(e) => setValor(e.target.value)}
+      />
+      <input type="button" value="Cadastrar" onClick={() => create(objetivo)} />
     </div>
-  )
+  );
 }
 
-export default Input
+export default Input;
